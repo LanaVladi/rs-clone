758714fe327ad6df5b6f544de93b3c0401b4b7e5
Svetlana <lanavladi1605gmail.com>
2023-02-06 22:32:18 +0500

    fix: add  router to src/view/RenderView.ts

--- src/view/RenderView.ts
+++ src/view/RenderView.ts
@@ -1,6 +1,6 @@
 import { FooterController } from '../controller/FooterController';
 import { HeaderController } from '../controller/HeaderController';
-import { WeatherTodayController } from '../controller/WeatherTodayController';
+import { Router } from './Router';
 
 class RenderView {
     constructor() {
@@ -9,11 +9,15 @@ class RenderView {
         const main = document.createElement('main');
         main.classList.add('main');
 
-        const headerController = new HeaderController();
-        const weatherTodayController = new WeatherTodayController();
+        const router = new Router(main);
+        const headerController = new HeaderController(router);
         const footerController = new FooterController();
 
-        root.append(headerController.component.element, main, weatherTodayController.component.element, footerController.component.element);
+        root.append(
+            headerController.component.element,
+            main,
+            footerController.component.element
+        );
         document.body.append(root);
     }
 }
