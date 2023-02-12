167762adda0017ae9cd99b9a8006971fa9169122
Svetlana <lanavladi1605gmail.com>
2023-02-13 01:28:43 +0500

    feat: add src/constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+export const apiKeyMapForecast = 'xXzWFyvlQ5v6hx3VoAyja2ss9hpFgVRi'; // windy.com Map Forecast API
+export const baseLinkMapForecast = `https://api.windy.com/assets/map-forecast/`;
+
+export const apiKeyOpenWeather = 'cb5257dbb6329ba1752a8bea5ea623e5'; // openweathermap.org
+export const baseLinkOpenWeather = 'https://api.openweathermap.org/data/2.5/weather'; //openweathermap.org
+
+export const apiKeyPointForecast = ''; // windy.com Point Forecast API
+export const baseLinkPointForecast = 'https://api.windy.com/api/point-forecast/v2';
+
+export const celsius = 'metric';
+export const fahrenheit = 'imperial';
+export const lang = 'en';
