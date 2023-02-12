159b41acc36613421ae22ad460b4e7d6d5e450df
Svetlana <lanavladi1605gmail.com>
2023-02-13 02:08:54 +0500

    feat: add src/controller/WeatherMapPageController.ts

---
+++ src/controller/WeatherMapPageController.ts
@@ -0,0 +1,11 @@
+// import { Router } from "../view/Router";
+// import { BaseController } from "./BaseController";
+
+// export class WeatherMapPageController extends BaseController<WeatherMapPageComponent> {
+//     public component: WeatherMapPageComponent;
+
+//     constructor(router: Router) {
+//         super();
+//         this.component = new WeatherMapPageComponent(this, router);
+//     }
+// }
