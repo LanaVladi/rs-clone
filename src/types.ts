bde214f52bc20f08818d26d02547fb9700d75a57
Svetlana <lanavladi1605gmail.com>
2023-02-14 12:37:22 +0500

    fix: add src/types.ts

--- src/types.ts
+++ src/types.ts
@@ -43,3 +43,62 @@ export type WeatherTodayData = {
     };
     name: string;
 };
+
+export type weatherFiveDaysData = {
+    // cod: '200';
+    // message: 0;
+    // cnt: 40;
+    list: Array<weatherOneDayData>;
+    city: {
+        // id: 3163858;
+        name: string;
+        coord: {
+            lat: number;
+            lon: number;
+        };
+        // country: 'IT';
+        population: number;
+        timezone: number;
+        sunrise: number;
+        sunset: number;
+    };
+};
+
+export type weatherOneDayData = {
+    main: {
+        temp: number;
+        feels_like: number;
+        temp_min: number;
+        temp_max: number;
+        pressure: number;
+        sea_level: number;
+        grnd_level: number;
+        humidity: number;
+        temp_kf: number; // what is it?
+    };
+    weather: [
+        {
+            // id: 500;
+            main: string;
+            description: string;
+            icon: string;
+        }
+    ];
+    clouds: {
+        all: number;
+    };
+    wind: {
+        speed: number;
+        deg: number;
+        gust: number;
+    };
+    visibility: number;
+    // pop: 0.32;
+    rain: {
+        '3h': number; //0.26;
+    };
+    sys: {
+        pod: string; //   pod: 'd';
+    };
+    // dt_txt: '2022-08-30 15:00:00';
+};
