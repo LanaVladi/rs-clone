91d1e7a3e99c99f54a0d080cbff4746b61b82895
Svetlana <lanavladi1605gmail.com>
2023-02-14 12:47:07 +0500

    refactor: src/controller/WeatherMapPageController.ts

--- src/controller/WeatherMapPageController.ts
+++ src/controller/WeatherMapPageController.ts
@@ -1,11 +1,13 @@
-// import { Router } from "../view/Router";
-// import { BaseController } from "./BaseController";
+import { ObserverToView } from '../model/ObserverToView';
+import { WeatherMapPageComponent } from '../view/pages/weather-map-page/WeatherMapPageComponent';
+import { Router } from "../view/Router";
+import { BaseController } from "./BaseController";
 
-// export class WeatherMapPageController extends BaseController<WeatherMapPageComponent> {
-//     public component: WeatherMapPageComponent;
+export class WeatherMapPageController extends BaseController<WeatherMapPageComponent> {
+    public component: WeatherMapPageComponent;
 
-//     constructor(router: Router) {
-//         super();
-//         this.component = new WeatherMapPageComponent(this, router);
-//     }
-// }
+    constructor(router: Router, observerToView: ObserverToView) {
+        super();
+        this.component = new WeatherMapPageComponent(this, router, observerToView);
+    }
+}
