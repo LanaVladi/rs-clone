8197c28b7195ee0295b758f4cf9c74148dd2dfd2
Svetlana <lanavladi1605gmail.com>
2023-02-18 01:59:00 +0500

    fix: src/view/pages/air-quality-page/AirQualityPageComponent.ts

--- src/view/pages/air-quality-page/AirQualityPageComponent.ts
+++ src/view/pages/air-quality-page/AirQualityPageComponent.ts
@@ -1,34 +1,55 @@
-import { lang, langObj } from '../../../constants';
 import { AirQualityPageController } from '../../../controller/AirQualityPageController';
 import { ObserverToView } from '../../../model/ObserverToView';
-import { NotifyParameters, WeatherTodayData } from '../../../types';
+import { TranslatorModel } from '../../../model/TranslatorModel';
+import INotify, { ModelEvent, NotifyParameters, pagesLang } from '../../../types';
 import { BaseComponent } from '../../BaseComponent';
 import { Router } from '../../Router';
 
 interface AirQualityPageComponentProps {
     controller: AirQualityPageController;
     router: Router;
+    language: TranslatorModel;
 }
 
-export class AirQualityPageComponent extends BaseComponent<AirQualityPageComponentProps> {
+export class AirQualityPageComponent extends BaseComponent<AirQualityPageComponentProps> implements INotify {
     private title!: HTMLHeadingElement;
     private observerToView: ObserverToView;
+    private language: TranslatorModel;
 
-    constructor(controller: AirQualityPageController, router: Router, observerToView: ObserverToView) {
-        super('other-forecast-air-quality', { controller, router }, 'div');
+    constructor(
+        controller: AirQualityPageController,
+        router: Router,
+        observerToView: ObserverToView,
+        language: TranslatorModel
+    ) {
+        super('other-forecast-air-quality', { controller, router, language }, 'div');
         this.observerToView = observerToView;
-        this.observerToView.subscribe(<T>(params: NotifyParameters<T>) => this.setWeatherIndicatorsOther(params));
+        this.language = language;
+        this.observerToView.subscribe(ModelEvent.air_quality_forecast_indicators, this);
     }
 
-    setWeatherIndicatorsOther<T>(params: NotifyParameters<T>) {
-        const weatherData = <WeatherTodayData>params.message;
-        this.title.innerText = `${langObj[lang].airQualityToday}:`;
+    notify<T>(params: NotifyParameters<T>): void {
+        switch (params.typeEvents) {
+            case ModelEvent.language: {
+                const langObject = <pagesLang>params.message;
+
+                break;
+            }
+            case ModelEvent.five_days_weather_indicators: {
+                // const {
+                //     temp,
+                // } = <weatherIndicators>params.message;
+                // this.title.innerText = `Качество воздуха сегодня: ${weatherData.list[0].components.co}`;
+                // this.title.innerText = `Качество воздуха сегодня: ${weatherData.list[0].components.nh3}`;
+                // this.title.innerText = `Качество воздуха сегодня: ${weatherData.list[0].components.no}`;
+            }
+        }
     }
 
     protected render(): void {
         this.title = document.createElement('h2');
         this.title.className = 'component__title';
-        this.title.innerText = `${langObj[lang].airQualityToday}:`;
+        this.title.innerText = `Качество воздуха сегодня: `;
 
         this.element.append(this.title);
     }
