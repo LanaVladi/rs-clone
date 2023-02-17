6e583723a457e7bb645653ae1cfdf6dcdef98497
Svetlana <lanavladi1605gmail.com>
2023-02-18 01:50:26 +0500

    fix: src/controller/HeaderController.ts

--- src/controller/HeaderController.ts
+++ src/controller/HeaderController.ts
@@ -4,14 +4,35 @@ import { SearcherController } from './SearchController';
 import { ObserverToModel } from '../model/ObserverToModel';
 import { ObserverToView } from '../model/ObserverToView';
 import { HeaderComponent } from '../view/components/header/HeaderComponent';
+import { GeolocationController } from './GeolocationController';
+import { GeolocationModel } from '../model/GeolocationModel';
+import { LanguageController } from './LanguageController';
+import { TranslatorModel } from '../model/TranslatorModel';
+import { TempUnitController } from './TempUnitController';
 
 export class HeaderController extends BaseController<HeaderComponent> {
     public component: HeaderComponent;
     public searcherController: SearcherController;
+    public languageController: LanguageController;
+    public geolocationController: GeolocationController;
+    public tempUnitController: TempUnitController;
+    private geolocation: GeolocationModel;
+    private language: TranslatorModel;
 
-    constructor(router: Router, observerToModel: ObserverToModel, observerToView: ObserverToView) {
+    constructor(
+        router: Router,
+        observerToModel: ObserverToModel,
+        observerToView: ObserverToView,
+        geolocation: GeolocationModel,
+        language: TranslatorModel
+    ) {
         super();
-        this.searcherController = new SearcherController(observerToModel);
-        this.component = new HeaderComponent(this, router, observerToView);
+        this.geolocation = geolocation;
+        this.language = language;
+        this.languageController = new LanguageController(observerToModel);
+        this.searcherController = new SearcherController(observerToModel, observerToView, language);
+        this.tempUnitController = new TempUnitController(observerToModel);
+        this.geolocationController = new GeolocationController(observerToModel, geolocation);
+        this.component = new HeaderComponent(this, router, observerToView, language);
     }
 }
