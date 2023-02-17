bb513c10da0a7777a4f99e940219fc35c969e6db
Svetlana <lanavladi1605gmail.com>
2023-02-17 21:48:13 +0500

    fix: add lang src/view/components/header/SearchComponent.ts

--- src/view/components/header/SearcherComponent.ts
+++ src/view/components/header/SearcherComponent.ts
@@ -1,28 +1,52 @@
-import { langObj, lang } from '../../../constants';
+// import { langObj, lang } from '../../../constants';
 import { SearcherController } from '../../../controller/SearchController';
 import { ObserverToModel } from '../../../model/ObserverToModel';
-import { APIEvent } from '../../../types';
+import { ObserverToView } from '../../../model/ObserverToView';
+import { TranslatorModel } from '../../../model/TranslatorModel';
+import { ModelEvent, NotifyParameters, pagesLang, ViewEvent } from '../../../types';
 import { BaseComponent } from '../../BaseComponent';
 
 interface SearcherComponentProps {
     controller: SearcherController;
     observerToModel: ObserverToModel;
+    observerToView: ObserverToView;
+    language: TranslatorModel;
 }
 export class SearcherComponent extends BaseComponent<SearcherComponentProps> {
     public searchInput!: HTMLInputElement;
     public searchIcon!: HTMLDivElement;
     private observerToModel: ObserverToModel;
+    private observerToView: ObserverToView;
+    private language: TranslatorModel;
 
-    constructor(controller: SearcherController, observerToModel: ObserverToModel) {
-        super('searcher-container', { controller, observerToModel }, 'div');
+    constructor(
+        controller: SearcherController,
+        observerToModel: ObserverToModel,
+        observerToView: ObserverToView,
+        language: TranslatorModel
+    ) {
+        super('searcher-container', { controller, observerToModel, observerToView, language }, 'div');
         this.observerToModel = observerToModel;
+        this.observerToView = observerToView;
+        this.language = language;
+        this.observerToView.subscribe(ModelEvent.language, this);
+    }
+
+    notify<T>(params: NotifyParameters<T>): void {
+        switch (params.typeEvents) {
+            case ModelEvent.language: {
+                const langObject = <pagesLang>params.message;
+                this.searchInput.placeholder = langObject.searchByCity;
+                break;
+            }
+        }
     }
 
     protected render(): void {
         this.searchInput = document.createElement('input');
         this.searchInput.classList.add('searcher-input');
         this.searchInput.setAttribute('autofocus', 'autofocus');
-        this.searchInput.placeholder = langObj[lang].searchByCity;
+        this.searchInput.placeholder = 'Поиск по городу';
 
         this.searchIcon = document.createElement('div');
         this.searchIcon.className = 'searcher-icon';
@@ -33,10 +57,7 @@ export class SearcherComponent extends BaseComponent<SearcherComponentProps> {
     protected addListeners(): void {
         this.searchInput.addEventListener('change', () => {
             const cityName = (this.searchInput as HTMLInputElement).value;
-            this.observerToModel.notify({
-                message: cityName,
-                typeEvents: APIEvent.todayWeather || APIEvent.fiveDaysWeather,
-            });
+            this.observerToModel.notify(ViewEvent.input, { message: cityName, typeEvents: ModelEvent.input });
         });
     }
 }
