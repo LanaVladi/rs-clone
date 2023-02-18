fa7cfdb0a40049c4e215216343fa472f7ec146d4
Ilya Hancharuk <innot707@gmail.com>
2023-02-18 14:15:27 +0300

    feat: add styles to map

--- src/view/pages/weather-map-page/altitudeInput.ts
+++ src/view/pages/weather-map-page/altitudeInput.ts
@@ -1,10 +1,12 @@
 import { lang, langObj, textValuesToAltitude } from '../../../constants';
 import { IStore } from '../../../types';
 
+const WIDTH_INPUT = 11.1;
+
 class AltitudeInput {
     private element: HTMLDivElement = document.createElement('div');
     private altitudeRange: HTMLInputElement = document.createElement('input');
-    private altitudeValue: HTMLSpanElement = document.createElement('span');
+    private altitudeValue: HTMLLabelElement = document.createElement('label');
 
     constructor(store: IStore) {
         this.render(store);
@@ -13,13 +15,16 @@ class AltitudeInput {
     private render(store: IStore) {
         this.element.className = 'altitude-input-container';
         this.altitudeRange.type = 'range';
+        this.altitudeRange.id = 'altitude';
         this.altitudeRange.min = '0';
         this.altitudeRange.max = '15';
         this.altitudeRange.value = '0';
         this.altitudeRange.className = 'altitude-range';
 
         this.altitudeValue.className = 'altitude-value';
+        this.altitudeValue.htmlFor = 'altitude';
         this.altitudeValue.innerText = langObj[lang].surface;
+        this.altitudeValue.style.top = `11.1rem`;
 
         this.element.append(this.altitudeRange, this.altitudeValue);
 
@@ -40,6 +45,7 @@ class AltitudeInput {
             this.altitudeRange.style.backgroundSize = `${(val * 100) / max}% 100%`;
             const valuesList = lang === 'en' ? Object.keys(textValuesToAltitude) : Object.values(textValuesToAltitude);
             this.altitudeValue.innerText = valuesList[+this.altitudeRange.value];
+            this.altitudeValue.style.top = `${(WIDTH_INPUT - (WIDTH_INPUT / max) * val)}rem`;
         });
     }
 
