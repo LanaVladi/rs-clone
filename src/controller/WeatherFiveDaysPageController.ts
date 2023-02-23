298c22c4ecae7e4a6c7d7131c292c371f6acbe28
Svetlana <lanavladi1605gmail.com>
2023-02-23 13:02:09 +0500

    fix: add src/controller/WeatherFiveDaysPageController.ts

--- src/controller/WeatherFiveDaysPageController.ts
+++ src/controller/WeatherFiveDaysPageController.ts
@@ -3,12 +3,13 @@ import { Router } from '../view/Router';
 import { ObserverToView } from '../model/ObserverToView';
 import { WeatherFiveDaysPageComponent } from '../view/pages/5-days-weather-page/WeatherFiveDaysPageComponent';
 import { TranslatorModel } from '../model/TranslatorModel';
+import { ObserverToModel } from '../model/ObserverToModel';
 
 export class WeatherFiveDaysPageController extends BaseController<WeatherFiveDaysPageComponent> {
     public component: WeatherFiveDaysPageComponent;
     private language: TranslatorModel;
 
-    constructor(router: Router, observerToView: ObserverToView, language: TranslatorModel) {
+    constructor(router: Router,observerToModel: ObserverToModel, observerToView: ObserverToView, language: TranslatorModel) {
         super();
         this.language = language;
         this.component = new WeatherFiveDaysPageComponent(this, router, observerToView, language);
